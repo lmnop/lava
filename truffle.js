b6241b2f8d46873091991981c85db33219df06b9
bpeters <brennen.peters@gmail.com>
2017-06-15 13:21:58 +0100

    add from params

--- truffle.js
+++ truffle.js
@@ -9,11 +9,13 @@ module.exports = {
       host: 'localhost',
       port: 8546,
       network_id: "*",
+      from: process.env.TEST_ADDRESS,
     },
     live: {
       host: 'localhost',
       port: 8547,
       network_id: 1,
+      from: process.env.LIVE_ADDRESS,
     },
   }
 };
