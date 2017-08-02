055612c8872162af8f1e48215083d1ba0070392f
bpeters <brennen.peters@gmail.com>
2017-08-02 01:08:04 -0500

    fix deploy

--- dapp/deploy.sh
+++ dapp/deploy.sh
@@ -2,6 +2,8 @@
 
 yarn build &&
 cd build &&
+git init &&
+git remote add origin https://github.com/lmnop/lava-dapp.git &&
 git add -A &&
 git commit -m "new release" &&
-git push
+git push --set-upstream origin master
